75eeab77fa27e5b780792bff952c85c99bea082e
Henry Robert Muwanika <hrmuwanika@gmail.com>
2024-12-30 05:02:29 +0200

    Update install_odoo_ubuntu.sh

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -67,6 +67,10 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
 #--------------------------------------------------
 # Set up the timezones
 #--------------------------------------------------
@@ -95,13 +99,12 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Python Dependencies
 #--------------------------------------------------
 echo -e "\n=================== Installing Python Dependencies ============================"
-sudo apt install -y git wget python3 python3-dev python3-pip python3-wheel libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential \
-libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev libzip-dev python3-setuptools node-less \
-python3-venv python3-cffi gdebi zlib1g-dev curl
+sudo apt install -y git wget python3.10 python3.10-dev python3.10-pip python3.10-wheel libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential \
+libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev libzip-dev python3.10-setuptools node-less \
+python3.10-venv python3.10-cffi gdebi zlib1g-dev curl cython3
 
-pip3 install --upgrade pip
-pip3 install gevent
-pip3 install python-ldap
+sudo pip3 install --upgrade pip
+sudo pip3 install setuptools wheel gevent greenlet python-ldap
 
 echo -e "\n================== Install Wkhtmltopdf ============================================="
 sudo apt -y install xfonts-75dpi xfonts-encodings xfonts-utils xfonts-base fontconfig
