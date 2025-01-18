52bfe1cb0c84672b4abd870000dbf6651884dfdf
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-01-18 18:37:32 +0200

    Update install_odoo_ubuntu.sh

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -101,7 +101,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 echo -e "\n=================== Installing Python Dependencies ============================"
 sudo apt install -y git wget python3 python3-dev python3-pip python3-wheel libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential \
 libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev libzip-dev python3-setuptools node-less \
-python3-venv python3-cffi gdebi zlib1g-dev curl cython3
+python3-venv python3-cffi gdebi zlib1g-dev curl cython3 python3-openssl
 
 sudo pip3 install --upgrade pip --break-system-packages
 sudo pip3 install setuptools wheel --break-system-packages
